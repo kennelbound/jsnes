29138c31d00eac227668b6255959977a15c94ee1
Conrad Kramer <conrad@kramerapps.com>
2014-02-15 03:26:49 -0500

    Add setButton function on Input

--- source/input.js
+++ source/input.js
@@ -20,15 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 JSNES.Input = function() {
     var i;
     
-    this.keys = {
-        KEY_A: 0,
-        KEY_B: 1,
-        KEY_SELECT: 2,
-        KEY_START: 3,
-        KEY_UP: 4,
-        KEY_DOWN: 5,
-        KEY_LEFT: 6,
-        KEY_RIGHT: 7
+    this.buttons = {
+        BUTTON_A: 0,
+        BUTTON_B: 1,
+        BUTTON_SELECT: 2,
+        BUTTON_START: 3,
+        BUTTON_UP: 4,
+        BUTTON_DOWN: 5,
+        BUTTON_LEFT: 6,
+        BUTTON_RIGHT: 7
     };
     this.gamepads = [];
     this.activeGamepads = false;
@@ -47,51 +47,27 @@ JSNES.Input.prototype = {
     setKey: function(key, value) {
         if (this.activeGamepads) return false;
         switch (key) {
-            case 88: this.state1[this.keys.KEY_A] = value; break;      // X
-            case 89: this.state1[this.keys.KEY_B] = value; break;      // Y (Central European keyboard)
-            case 90: this.state1[this.keys.KEY_B] = value; break;      // Z
-            case 17: this.state1[this.keys.KEY_SELECT] = value; break; // Right Ctrl
-            case 13: this.state1[this.keys.KEY_START] = value; break;  // Enter
-            case 38: this.state1[this.keys.KEY_UP] = value; break;     // Up
-            case 40: this.state1[this.keys.KEY_DOWN] = value; break;   // Down
-            case 37: this.state1[this.keys.KEY_LEFT] = value; break;   // Left
-            case 39: this.state1[this.keys.KEY_RIGHT] = value; break;  // Right
+            case 88: this.state1[this.buttons.BUTTON_A] = value; break;      // X
+            case 89: this.state1[this.buttons.BUTTON_B] = value; break;      // Y (Central European keyboard)
+            case 90: this.state1[this.buttons.BUTTON_B] = value; break;      // Z
+            case 17: this.state1[this.buttons.BUTTON_SELECT] = value; break; // Right Ctrl
+            case 13: this.state1[this.buttons.BUTTON_START] = value; break;  // Enter
+            case 38: this.state1[this.buttons.BUTTON_UP] = value; break;     // Up
+            case 40: this.state1[this.buttons.BUTTON_DOWN] = value; break;   // Down
+            case 37: this.state1[this.buttons.BUTTON_LEFT] = value; break;   // Left
+            case 39: this.state1[this.buttons.BUTTON_RIGHT] = value; break;  // Right
 
-            case 103: this.state2[this.keys.KEY_A] = value; break;     // Num-7
-            case 105: this.state2[this.keys.KEY_B] = value; break;     // Num-9
-            case 99: this.state2[this.keys.KEY_SELECT] = value; break; // Num-3
-            case 97: this.state2[this.keys.KEY_START] = value; break;  // Num-1
-            case 104: this.state2[this.keys.KEY_UP] = value; break;    // Num-8
-            case 98: this.state2[this.keys.KEY_DOWN] = value; break;   // Num-2
-            case 100: this.state2[this.keys.KEY_LEFT] = value; break;  // Num-4
-            case 102: this.state2[this.keys.KEY_RIGHT] = value; break; // Num-6
+            case 103: this.state2[this.buttons.BUTTON_A] = value; break;     // Num-7
+            case 105: this.state2[this.buttons.BUTTON_B] = value; break;     // Num-9
+            case 99: this.state2[this.buttons.BUTTON_SELECT] = value; break; // Num-3
+            case 97: this.state2[this.buttons.BUTTON_START] = value; break;  // Num-1
+            case 104: this.state2[this.buttons.BUTTON_UP] = value; break;    // Num-8
+            case 98: this.state2[this.buttons.BUTTON_DOWN] = value; break;   // Num-2
+            case 100: this.state2[this.buttons.BUTTON_LEFT] = value; break;  // Num-4
+            case 102: this.state2[this.buttons.BUTTON_RIGHT] = value; break; // Num-6
             default: return true;
         }
         return false; // preventDefault
-    },
-
-    pollGamepads: function() {
-        var index = -1;
-        var active = false;
-        for (var i = 0; i < this.gamepads.length; i++) {
-            var gamepad = this.gamepads[i];
-            if (!gamepad || !gamepad.connected) continue;
-            index++;
-            active = true;
-            var buttons = gamepad.buttons;
-            var axes = gamepad.axes;
-            var state = index === 0 ? this.state1 : (index === 1 ? this.state2 : undefined);
-            if (!state) break;
-            state[this.keys.KEY_A] = (buttons[1].pressed || buttons[3].pressed) ? 0x41 : 0x40;
-            state[this.keys.KEY_B] = (buttons[0].pressed || buttons[2].pressed) ? 0x41 : 0x40;
-            state[this.keys.KEY_SELECT] = buttons[8].pressed ? 0x41 : 0x40;
-            state[this.keys.KEY_START] = buttons[9].pressed ? 0x41 : 0x40;
-            state[this.keys.KEY_UP] = buttons[12].pressed ? 0x41 : 0x40;
-            state[this.keys.KEY_DOWN] = buttons[13].pressed ? 0x41 : 0x40;
-            state[this.keys.KEY_LEFT] = buttons[14].pressed ? 0x41 : 0x40;
-            state[this.keys.KEY_RIGHT] = buttons[15].pressed ? 0x41 : 0x40;
-        }
-        this.activeGamepads = active;
     },
 
     keyDown: function(evt) {
@@ -108,6 +84,34 @@ JSNES.Input.prototype = {
     
     keyPress: function(evt) {
         evt.preventDefault();
+    },
+
+    setButton: function(button, value) {
+        this.state1[button] = value ? 0x41 : 0x40;
+    },
+
+    pollGamepads: function() {
+        var index = -1;
+        var active = false;
+        for (var i = 0; i < this.gamepads.length; i++) {
+            var gamepad = this.gamepads[i];
+            if (!gamepad || !gamepad.connected) continue;
+            index++;
+            active = true;
+            var buttons = gamepad.buttons;
+            var axes = gamepad.axes;
+            var state = index === 0 ? this.state1 : (index === 1 ? this.state2 : undefined);
+            if (!state) break;
+            state[this.buttons.BUTTON_A] = (buttons[1].pressed || buttons[3].pressed) ? 0x41 : 0x40;
+            state[this.buttons.BUTTON_B] = (buttons[0].pressed || buttons[2].pressed) ? 0x41 : 0x40;
+            state[this.buttons.BUTTON_SELECT] = buttons[8].pressed ? 0x41 : 0x40;
+            state[this.buttons.BUTTON_START] = buttons[9].pressed ? 0x41 : 0x40;
+            state[this.buttons.BUTTON_UP] = buttons[12].pressed ? 0x41 : 0x40;
+            state[this.buttons.BUTTON_DOWN] = buttons[13].pressed ? 0x41 : 0x40;
+            state[this.buttons.BUTTON_LEFT] = buttons[14].pressed ? 0x41 : 0x40;
+            state[this.buttons.BUTTON_RIGHT] = buttons[15].pressed ? 0x41 : 0x40;
+        }
+        this.activeGamepads = active;
     },
 
     gamepadConnected: function(gamepad) {
